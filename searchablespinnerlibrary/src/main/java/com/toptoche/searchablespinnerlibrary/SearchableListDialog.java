b1b4d36a6002ba2746f051f0fba265b2f4977c69
cprasad7 <141231858+cprasad7@users.noreply.github.com>
2023-08-22 02:21:24 +0530

    Update SearchableListDialog.java

--- searchablespinnerlibrary/src/main/java/com/toptoche/searchablespinnerlibrary/SearchableListDialog.java
+++ searchablespinnerlibrary/src/main/java/com/toptoche/searchablespinnerlibrary/SearchableListDialog.java
@@ -90,13 +90,13 @@ public class SearchableListDialog extends DialogFragment implements
         View rootView = inflater.inflate(R.layout.searchable_list_dialog, null);
         setData(rootView);
 
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+        alertDialog.setView(rootView);
+
         _searchView.requestFocus();
 
         InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.showSoftInput(_searchView, InputMethodManager.SHOW_IMPLICIT);
-
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
-        alertDialog.setView(rootView);
 
         String strPositiveButton = _strPositiveButtonText == null ? "CLOSE" : _strPositiveButtonText;
         alertDialog.setPositiveButton(strPositiveButton, _onClickListener);
