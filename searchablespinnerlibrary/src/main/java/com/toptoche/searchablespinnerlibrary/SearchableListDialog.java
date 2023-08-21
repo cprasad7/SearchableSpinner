17dfd095559299238b8c140d915814cdc78133e8
cprasad7 <141231858+cprasad7@users.noreply.github.com>
2023-08-21 21:11:47 +0530

    Update SearchableListDialog.java

--- searchablespinnerlibrary/src/main/java/com/toptoche/searchablespinnerlibrary/SearchableListDialog.java
+++ searchablespinnerlibrary/src/main/java/com/toptoche/searchablespinnerlibrary/SearchableListDialog.java
@@ -17,6 +17,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import androidx.appcompat.widget.SearchView;
+import android.widget.EditText;
+import android.view.inputmethod.InputMethodManager;
 
 import java.io.Serializable;
 import java.util.List;
@@ -146,9 +149,15 @@ public class SearchableListDialog extends DialogFragment implements
         _searchView.setIconifiedByDefault(false);
         _searchView.setOnQueryTextListener(this);
         _searchView.setOnCloseListener(this);
-        _searchView.clearFocus();
-        InputMethodManager mgr = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-        mgr.hideSoftInputFromWindow(_searchView.getWindowToken(), 0);
+        // Get reference to the EditText inside the SearchView
+    EditText searchEditText = _searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+
+    // Request focus on the EditText
+    searchEditText.requestFocus();
+
+    // Open the soft keyboard
+    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+    imm.showSoftInput(searchEditText, InputMethodManager.SHOW_IMPLICIT);
 
         List items = (List) getArguments().getSerializable(ITEMS);
 
